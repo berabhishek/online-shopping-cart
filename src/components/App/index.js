37493bea548e7fd275e47c63d19ace01522224bf
Abhishek Bera <abhisecre@gmail.com>
2019-12-26 19:07:28 +0530

    init commit

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Shelf from '../Shelf';
+import Filter from '../Shelf/Filter';
+import FloatCart from '../FloatCart';
+
+const App = () => (
+  <React.Fragment>
+    <main>
+      <Filter />
+      <Shelf />
+    </main>
+    <FloatCart />
+  </React.Fragment>
+);
+
+export default App;
